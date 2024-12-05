2b128a532622670d1038fef9c9edf39d7147768a
carlos-tecsup <carlos.huaynates@tecsup.edu.pe>
2024-12-04 23:12:19 -0500

    add license query and command microservices

--- common-models/src/main/java/com/tecsup/microservices/common_models/entity/DriverLicenseEntity.java
+++ common-models/src/main/java/com/tecsup/microservices/common_models/entity/DriverLicenseEntity.java
@@ -1,28 +1,38 @@
 package com.tecsup.microservices.common_models.entity;
 
+import com.tecsup.microservices.common_models.enums.LicenseCategory;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Builder
 @Entity
 @Table(name = "driver_license")
 @AllArgsConstructor
-@NoArgsConstructor
 public class DriverLicenseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    private Integer id;
-    private com.tecsup.microservices.common_models.enums.LicenseCategory LicenseCategory;
-    private Date dueDate;
-    private Date emitDate;
+    private int id;
+    private LicenseCategory licenseCategory;
     private boolean state;
+    private int documentNumber;
+    private String fullName;
+    private LocalDate dueDate;
+    private LocalDate emitDate;
+
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity userEntity;
+
+    public DriverLicenseEntity() {
+        this.dueDate = LocalDate.now();
+        this.emitDate = LocalDate.now().plusYears(3);
+        this.state = true;
+    }
 }
